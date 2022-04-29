856172962b90167628d83b454edc6a2e746fe7f2
walerio <81855066+valerio-bosco@users.noreply.github.com>
2022-04-30 00:24:46 +0200

    implementazione popup-page dei blocchi con responsiveness

--- js/MY_js/popup-page.js
+++ js/MY_js/popup-page.js
@@ -1,11 +1,23 @@
 $(document).ready(function(){
-    $("div[id|=block]").click(function(){
-        $("#mySidenav").show();
+    let w="0px"
+    $("div[id|=block]").click(function(){       //|Serve ad attivare la popup page per ogni blocco
         
+        if(window.innerWidth<425) w="97%";
+        else w="300px";
+        
+        $("#mySidenav").css("width",w);
     })
-    
-    $(".closebtn").click(function(){
-        $("#mySidenav").hide();
+
+    $(window).resize(function(){                //|Agisce nel caso vi sia un resize, e quindi per evitare che la width si mantenga al 97% agisce
+        if(window.innerWidth>=425 && w=="97%"){
+            w="0px";
+        }
+        $("#mySidenav").css("width",w);
+    })
+
+    $(".closebtn").click(function(){ 
+        w="0px"
+        $("#mySidenav").css("width",w);
 
     })
 
