3bdf0207225961d1292135817b8abb4098808f04
walerio <81855066+valerio-bosco@users.noreply.github.com>
2022-04-30 18:51:08 +0200

    aggiunto effetto di blur per dividere popup page da mosaico

--- js/MY_js/popup-page.js
+++ js/MY_js/popup-page.js
@@ -4,6 +4,10 @@ $(document).ready(function(){
     let z="-1;"
     let w_max="40%";
     let w_sm="97%"
+    let black_medium=" rgba(18, 18, 18, 0.500)";
+    let blur=false;
+
+    //Quando apro un blocco 
     $("div[id|=block]").click(function(){       //|Serve ad attivare la popup page per ogni blocco
         z="2";
         if(window.innerWidth<425) w=w_sm;
@@ -11,18 +15,25 @@ $(document).ready(function(){
        
         $(".info-pop").css("width",w);
         $("#pop-block").css("z-index",z);
-        $("#pop-block").css("background-color"," rgba(18, 18, 18, 0.500)");
+        $("#pop-block").css("background-color",black_medium);
+        $("div[class|=row]").toggleClass("filter");
+        blur=!blur;
     })
 
+    //Quando chiudo blocco
+    $(".closebtn").click(function(){ 
+        w="0px"
+        z="-1";
+        time="3s";
+        $(".info-pop").css("width",w);
+        $("#pop-block").animate({zIndex:z},1000);
+        $("#pop-block").css("background-color"," transparent");
+        $("div[class|=row]").toggleClass("filter");
+        blur=!blur;
+    })
+    //Responsivess
     $(window).resize(function(){                //|Agisce nel caso vi sia un resize, e quindi per evitare che la width si mantenga al w_sm% agisce
-       /* if( (window.innerWidth>=425 && w==w_sm) || (window.innerWidth<425 && w=="300px") ){
-            w="0px";
-            z="-1";
-            $(".info-pop").css("width",w);
-            $("#pop-block").css("z-index",z);
-            $("#pop-block").css("background-color"," rgba(18, 18, 18, 0.500)",2000);
-           return;
-        }*/
+        console.log(window.innerWidth + " blur "+ blur + "width:" + $(".info-pop").css("width"));
         if((window.innerWidth<425  && w==w_sm) ||  (window.innerWidth>=425 && w==w_max)){
             return
         }
@@ -30,18 +41,13 @@ $(document).ready(function(){
         z="-1";
         $(".info-pop").css("width",w);
         $("#pop-block").css("z-index",z);
-        $("#pop-block").css("background-color"," rgba(255, 255, 255, 0)");
-       
+        $("#pop-block").css("background-color","transparent");
+       if(blur) {
+            $("div[class|=row]").toggleClass("filter");
+            blur=!blur;
+        }
     })
 
-    $(".closebtn").click(function(){ 
-        w="0px"
-        z="-1";
-        time="3s";
-        $(".info-pop").css("width",w);
-        $("#pop-block").animate({zIndex:z},1000);
-        $("#pop-block").css("background-color"," rgba(255, 255, 255, 0)");
-       
-    })
+    
 
 });
