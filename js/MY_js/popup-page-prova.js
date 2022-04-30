3f90ee8a4e9e784dccf0f6aedbb531892bc5bc09
walerio <81855066+valerio-bosco@users.noreply.github.com>
2022-04-30 17:50:32 +0200

    implementata la popup page con responsiveness con scomparsa nel caso di allargamento/rimpicciolimento oltre i limiti

--- js/MY_js/popup-page-prova.js
+++ js/MY_js/popup-page-prova.js
@@ -2,43 +2,45 @@ $(document).ready(function(){
     
     let w="0px"
     let z="-1;"
-
+    let w_max="40%";
+    let w_sm="97%"
     $("div[id|=block]").click(function(){       //|Serve ad attivare la popup page per ogni blocco
         z="2";
-        if(window.innerWidth<425) w="97%";
-        else w="30%";
+        if(window.innerWidth<425) w=w_sm;
+        else w=w_max;
        
-        $(".sidenav").css("width",w);
-        $("#mySidenav").css("z-index",z);
-        $("#mySidenav").css("background-color"," rgba(18, 18, 18, 0.500)");
+        $(".info-pop").css("width",w);
+        $("#pop-block").css("z-index",z);
+        $("#pop-block").css("background-color"," rgba(18, 18, 18, 0.500)");
     })
 
-    $(window).resize(function(){                //|Agisce nel caso vi sia un resize, e quindi per evitare che la width si mantenga al 97% agisce
-       /* if( (window.innerWidth>=425 && w=="97%") || (window.innerWidth<425 && w=="300px") ){
+    $(window).resize(function(){                //|Agisce nel caso vi sia un resize, e quindi per evitare che la width si mantenga al w_sm% agisce
+       /* if( (window.innerWidth>=425 && w==w_sm) || (window.innerWidth<425 && w=="300px") ){
             w="0px";
             z="-1";
-            $(".sidenav").css("width",w);
-            $("#mySidenav").css("z-index",z);
-            $("#mySidenav").css("background-color"," rgba(18, 18, 18, 0.500)",2000);
+            $(".info-pop").css("width",w);
+            $("#pop-block").css("z-index",z);
+            $("#pop-block").css("background-color"," rgba(18, 18, 18, 0.500)",2000);
            return;
         }*/
-        if((window.innerWidth<425  && w=="97%") ||  (window.innerWidth>=425 && w=="30%")){
+        if((window.innerWidth<425  && w==w_sm) ||  (window.innerWidth>=425 && w==w_max)){
             return
         }
         w="0px";
         z="-1";
-        $(".sidenav").css("width",w);
-        $("#mySidenav").css("z-index",z);
-        $("#mySidenav").css("background-color"," rgba(255, 255, 255, 0)");
+        $(".info-pop").css("width",w);
+        $("#pop-block").css("z-index",z);
+        $("#pop-block").css("background-color"," rgba(255, 255, 255, 0)");
+       
     })
 
     $(".closebtn").click(function(){ 
         w="0px"
         z="-1";
         time="3s";
-        $(".sidenav").css("width",w);
-        $("#mySidenav").animate({zIndex:z},2000);
-        $("#mySidenav").css("background-color"," rgba(255, 255, 255, 0)");
+        $(".info-pop").css("width",w);
+        $("#pop-block").animate({zIndex:z},1000);
+        $("#pop-block").css("background-color"," rgba(255, 255, 255, 0)");
        
     })
 
