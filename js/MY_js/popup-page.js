869f9c093b76e1415b5ac507e1f049e5cb795848
walerio <81855066+valerio-bosco@users.noreply.github.com>
2022-05-01 22:14:53 +0200

    ordinato il popup-page.js

--- js/MY_js/popup-page.js
+++ js/MY_js/popup-page.js
@@ -7,16 +7,20 @@ $(document).ready(function(){
     let black_medium=" rgba(18, 18, 18, 0.500)";
     let blur=false;
 
+    function togglePopup(w,z,color){
+        $(".info-pop").css("width",w);
+        $("#pop-block").css("z-index",z);
+        $("#pop-block").css("background-color",color);
+    }
+
     //Quando apro un blocco 
     $("div[id|=block]").click(function(){       //|Serve ad attivare la popup page per ogni blocco
         //Design
         z="2";
         if(window.innerWidth<425) w=w_sm;
         else w=w_max;
-       
-        $(".info-pop").css("width",w);
-        $("#pop-block").css("z-index",z);
-        $("#pop-block").css("background-color",black_medium);
+    
+        togglePopup(w,z,black_medium)
         $("div[class|=row]").toggleClass("filter");
         blur=!blur;
         //
@@ -39,23 +43,23 @@ $(document).ready(function(){
         $("div[class|=row]").toggleClass("filter");
         blur=!blur;
     })
+    
     //Responsivess
     $(window).resize(function(){                //|Agisce nel caso vi sia un resize, e quindi per evitare che la width si mantenga al w_sm% agisce
         console.log(window.innerWidth + " blur "+ blur + "width:" + $(".info-pop").css("width"));
         if((window.innerWidth<425  && w==w_sm) ||  (window.innerWidth>=425 && w==w_max)){
-            
             return
         }
         w="0px";
         z="-1";
-        $(".info-pop").css("width",w);
-        $("#pop-block").css("z-index",z);
-        $("#pop-block").css("background-color","transparent");
-       if(blur) {
+        
+        togglePopup(w,z,"transparent");
+        if(blur) {
             $("div[class|=row]").toggleClass("filter");
             blur=!blur;
         }
     })
+    
 
     
 
