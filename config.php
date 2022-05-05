0081e53efbd311b0607e24487c7747224f2483af
Gianpaolo Busillo <83918379+gianbus@users.noreply.github.com>
2022-05-05 17:16:13 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php 
 
-    $username="Sql1634397";
-    $password="evS2fY55e3aUUW8jHnNNuyJvDr!";
-    $database="Sql1634397_1";
+    $username="root";
+    $password="";
+    $database="mosaic_db";
 	
-    $mysqli = mysqli_connect("89.46.111.132", $username, $password, $database);
+    $mysqli = mysqli_connect("localhost", $username, $password, $database);
     if(false === $mysqli){
         exit("Errore: impossibile stabilire una connessione " . mysqli_connect_error());
     }
-
-    echo "Connesso al database<br>";
     
 ?>
