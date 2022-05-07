7175e9632982d30daa371baffcfdf7ccf47edeff
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-07 19:32:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,17 +12,18 @@
 <body>
     <?php
 		include './config.php';
-	?>
+  	?>
     <div id="container" class="container-fluid">
     <h1>M O S A I C</h1>
 		<a href='./login'><h2>Testa il login!</h2></a><br>
 		<a href='./registrazione'><h2>Testa la registrazione!</h2></a><br>
 		<a href='./recuperapassword'><h2>Testa il recuperapassword!</h2></a><br>
     <a href='./logout'><h2>Testa il logout!</h2></a><br>
+    
     </div><!-- CONTAINER END -->
     <?php
 		include './footer.php';
-	?>
+  	?>
      <!-- Bootstrap Bundle with Popper -->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
