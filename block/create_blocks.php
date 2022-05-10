764c5bc3d04715f7c666886be42dbc5c75f3ac28
Gianpaolo Busillo <83918379+gianbus@users.noreply.github.com>
2022-05-10 14:40:15 +0200

    Update create_blocks.php

--- block/create_blocks.php
+++ block/create_blocks.php
@@ -1,9 +1,20 @@
 <?php
+    /*
+        Questa è solo un utility che si può utilizzare per creare blocchi
+        dall'id $MIN_BLOCK all'id $MAX_BLOCK, estremi compresi!
+        NB: Per far funzionare lo script bisogna essere loggati su mosaic
+        con l'account avente username 'mosaic'.
+        GB
+    */
+
+    $MIN_BLOCK = 0;
+    $MAX_BLOCK = 0;
+
     include './config.php';
 
     if(isset($_SESSION['utente']) && $_SESSION['utente']==mosaic){
         try{
-            for ($i = 1; $i <= 144; ++$i) {
+            for ($i = $MIN_BLOCK; $i <= $MAX_BLOCKS; ++$i) {
                 $crea_blocco = mysqli_query($mysqli, "INSERT INTO blocchi (id, proprietario, tipo, path) VALUES ('$i', 'mosaic', 'color', '#FFFFFF') ");
                 $crea_annuncio_blocco = mysqli_query($mysqli, "INSERT INTO market (idblocco, price) VALUES ('$i', 10) ");
                 if($crea_blocco && $crea_annuncio_blocco){
