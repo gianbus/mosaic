abc4f0d1bf3639a6971210b038e8f34988788bb4
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-16 22:38:46 +0200

    Logo cliccabile etc.

--- navbar.php
+++ navbar.php
@@ -3,23 +3,19 @@
     <div id="navbar">
         <div class="nav-menu nav-menu-resp ">
             <div id="logo">
-                <img src="/mosaic/assets/logos/logo2.png">
+                <img onclick="location.href = '/';" src="/mosaic/assets/logos/logo2.png">
             </div>
             <span style="flex: 2 2 auto; "></span>
-            <div id="home" >
-                <a class="nav-btn" href="/" >HOME</a>
-            </div>  
             
             <div id="profile" >
                 <?php
-                    //ai fini dei test in locale sono stati cambiati i valori di href nei tag a con /mosaic/...
                     if(!isset($_SESSION['utente'])){
-                      echo "\t\t\t\t\t\t<a class='signup nav-btn' href='/mosaic/registrazione' >_sign up</a>\n";
-                      echo "\t\t\t\t\t\t<a class='login nav-btn' href='/mosaic/login' >_login</a>\n";
+                      echo "\t\t\t\t\t\t<a class='signup nav-btn' href='/registrazione' >_sign up</a>\n";
+                      echo "\t\t\t\t\t\t<a class='login nav-btn' href='/login' >_login</a>\n";
                     }else{
-                      echo "\t\t\t\t\t\t<span id='points' class=' nav-btn'>".$_SESSION['punti']."</span>\n";
-                      echo "\t\t\t\t\t\t<a id='logged-username' class=' nav-btn' href='/mosaic/profile' >".$_SESSION['utente']."</a>\n";
-                      echo "\t\t\t\t\t\t<a class='login nav-btn' href='/mosaic/logout' >_logout</a>\n";
+                      echo "\t\t\t\t\t\t<span id='points' class=' nav-btn'><i class=\"fa fa-money\" aria-hidden=\"true\"></i>".$_SESSION['punti']."</span>\n";
+                      echo "\t\t\t\t\t\t<a id='logged-username' class=' nav-btn' href='/profile' >".$_SESSION['utente']."</a>\n";
+                      echo "\t\t\t\t\t\t<a class='login nav-btn' href='/logout' >_logout</a>\n";
                     }
                 ?> 
             </div>
