fc864e11ab1bf3da05fcffb020fe71f415290b0f
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-18 20:06:19 +0200

    Aggiunto bozza footer

--- footer.php
+++ footer.php
@@ -1,3 +1,16 @@
+<div id="footer">
+    <div id="footer-logo">
+        <span style="flex: 2 2 auto; "></span>
+        <img onclick="location.href = '/';" src="https://www.ltw-mosaic.it/assets/logos/logo2.png">
+        <span style="flex: 2 2 auto; "></span>
+    </div>
+    <div id="footer-credits">
+        <p class="credits">MOSAIC &#169; 2022</p>
+    </div>  
+</div>
+
+
+
 <!-- JQuery -->
 <script src="https://ltw-mosaic.it/jquery/jquery.min.js"></script>
 
