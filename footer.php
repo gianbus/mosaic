6fc6da48bf30c3421c1311e5c7773e214be09230
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-18 22:08:56 +0200

    Footer

--- footer.php
+++ footer.php
@@ -1,12 +1,17 @@
 <div id="footer">
+
     <div id="footer-logo">
-        <span style="flex: 2 2 auto; "></span>
-        <img onclick="location.href = '/';" src="https://www.ltw-mosaic.it/assets/logos/logo2.png">
-        <span style="flex: 2 2 auto; "></span>
+        <span></span>
+        <div>
+            <a href="/"><img src="https://www.ltw-mosaic.it/assets/logos/logo2.png"></a>
+        </div>
+        <span></span>
     </div>
+
     <div id="footer-credits">
         <p class="credits">MOSAIC &#169; 2022</p>
     </div>  
+
 </div>
 
 
