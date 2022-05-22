b72964568ce4d10a12c40c5427cdc71f28fe79b6
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-23 00:55:05 +0200

    Rick merge

--- js/verifica-campi-inseriti.js
+++ js/verifica-campi-inseriti.js
@@ -1,213 +1,234 @@
-let form = document.getElementById("formregistrazione");
 
-let msgNome = "Inserisci il tuo nome";
-let msgCognome = "Inserisci il tuo cognome";
-let msgUsername = "Inserisci il tuo username";
-let msgPassword1 = "Inserisci la tua password";
-let msgPassword2 = "Rinserisci la password";
-let msgCorrispondenzaPassword = "Le password devono corrispondere";
-let msgEmail = "Inserisci la tua email";
-let msgEmailInvalida = "Inserire un'email valida";
-let msgTerminiDiServizio = "Devi accettare i termini di servizio";
+   let form = document.getElementById("formregistrazione");
+
+    let msgNome = "Inserisci il tuo nome";
+    let msgCognome = "Inserisci il tuo cognome";
+    let msgUsername = "Inserisci il tuo username";
+    let msgPassword1 = "Inserisci la tua password";
+    let msgPassword2 = "Rinserisci la password";
+    let msgCorrispondenzaPassword = "Le password devono corrispondere";
+    let msgEmail = "Inserisci la tua email";
+    let msgEmailInvalida = "Inserire un'email valida";
+    let msgTerminiDiServizio = "Devi accettare i termini di servizio";
 
 
-function verificaForm() {
+    function verificaForm() {
 
-    if (!verificaCampiVuoti())
+        if (!verificaCampiVuoti())
+            return false;
+
+        else if (!verificaPassword())
+            return false;
+
+        else if (!verificaEmail())
+            return false;
+
+        else if (!(document.getElementById("checktermini").checked)) {
+            alert(msgTerminiDiServizio);
+            return false;
+        }
+
+        else if (document.getElementById("pass-debole").style.backgroundColor == "red") {
+            alert("La Password inserita non è abbastanza forte");
+            return false;
+        }
+        
+        return true;
+        
+    }
+
+    function verificaCampiVuoti() {
+        let res = 0;
+
+        if (form["nome"].value.trim() === "" || verificaNienteNumeri(form["nome"].value.trim())) {
+            showErrorMessage(form["nome"], msgNome);
+            res++;
+        }
+
+        if (form["cognome"].value.trim() === "" || verificaNienteNumeri(form["cognome"].value.trim())) {
+            showErrorMessage(form["cognome"], msgCognome);
+            res++;
+        }
+
+        if (form["username"].value.trim() === "") {
+            showErrorMessage(form["username"], msgUsername);
+            res++;
+        }
+
+
+        if (form["email"].value.trim() === "") {
+            showErrorMessage(form["email"], msgEmail);
+            res++;
+        }
+
+
+        if (form["password1"].value.trim() === "") {
+            showErrorMessage(form["password1"], msgPassword1);
+            res++;
+        }
+
+        if (form["password2"].value.trim() === "") {
+            showErrorMessage(form["password2"], msgPassword2);
+            res++;
+        }
+
+        
+
+        return res > 0 ? false : true;
+        
+    }
+
+    function verificaNienteNumeri(stringa) {
+        return /\d/.test(stringa);
+    }
+
+
+    function verificaPassword() {
+        if (form["password1"].value != form["password2"].value) {
+            form["password1"].value = "";
+            form["password2"].value = "";
+            return showErrorMessage(form["password2"], msgCorrispondenzaPassword);
+
+        }
+        
+        return true;
+    }
+
+    function verificaEmail() {
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        const e = form["email"];             //document.getElementById("email");
+        if (!emailRegex.test(e.value)) {
+            e.value = "";
+            return showErrorMessage(e, msgEmailInvalida);
+        }
+        
+        return true;
+    }
+
+
+    function showErrorMessage(elem, msg) {
+        //elem.parentNode.querySelector("small").innerHTML = msg;
+        //form.querySelectorAll("small")[num].innerHTML = msg + "<br>";
+        elem.placeholder = msg;
+        elem.className = "error";
         return false;
-
-    else if (!verificaPassword())
-        return false;
-
-    else if (!verificaEmail())
-        return false;
-
-    else if (!(document.getElementById("checktermini").checked)) {
-        alert(msgTerminiDiServizio);
-        return false;
     }
 
-    else if (document.getElementById("pass-debole").style.backgroundColor == "red") {
-        alert("La Password inserita non è abbastanza forte");
-        return false;
-    }
-    
-    return true;
-    
-}
 
-function verificaCampiVuoti() {
-    let res = 0;
+    function auxBordiNeri(inputElem) {
+        if (inputElem.className == "error") {
+            inputElem.parentNode.querySelector("small").innerText = "";
+            inputElem.className = "";
+        }
 
-    if (form["nome"].value.trim() === "" || verificaNienteNumeri(form["nome"].value.trim())) {
-        showErrorMessage(form["nome"], msgNome);
-        res++;
+        return;
     }
 
-    if (form["cognome"].value.trim() === "" || verificaNienteNumeri(form["cognome"].value.trim())) {
-        showErrorMessage(form["cognome"], msgCognome);
-        res++;
+
+    let campiInput = document.getElementsByTagName("input");
+
+    for (let i=0; i < campiInput.length-2; i++) {
+        console.log(campiInput[i]);
+        campiInput[i].addEventListener("click", () => {
+            auxBordiNeri(campiInput[i]);
+        });
     }
 
-    if (form["username"].value.trim() === "") {
-        showErrorMessage(form["username"], msgUsername);
-        res++;
+
+    document.getElementById("mostra").addEventListener("click", mostraPassword);
+
+    function mostraPassword() {
+        let p1 = $("#password1");
+        let p2 = $("#password2");
+        let mostra = $("#mostra");
+
+        
+        if (p1.attr("type") === "password") {
+            p1.attr("type","text");
+            p2.attr("type","text");
+            mostra.removeClass("fa-eye");
+            mostra.addClass("fa-eye-slash");
+        } 
+        
+        else {
+            p1.attr("type","password");
+            p2.attr("type","password");
+            
+            mostra.removeClass("fa-eye-slash");
+            mostra.addClass("fa-eye");
+        
+        }
+        
+        return;
     }
 
-    if (form["password1"].value.trim() === "") {
-        showErrorMessage(form["password1"], msgPassword1);
-        res++;
-    }
 
-    if (form["password2"].value.trim() === "") {
-        showErrorMessage(form["password2"], msgPassword2);
-        res++;
-    }
+    $("#password1").keyup(validitaPassword);
 
-    if (form["email"].value.trim() === "") {
-        showErrorMessage(form["email"], msgEmail);
-        res++;
-    }
-
-    return res > 0 ? false : true;
-    
-}
-
-function verificaNienteNumeri(stringa) {
-    return /\d/.test(stringa);
-}
-
-
-function verificaPassword() {
-    if (form["password1"].value != form["password2"].value) 
-        return showErrorMessage(form["password2"], msgCorrispondenzaPassword);
-    
-    return true;
-}
-
-function verificaEmail() {
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    const e = form["email"];             //document.getElementById("email");
-    if (!emailRegex.test(e.value)) 
-        return showErrorMessage(e, msgEmailInvalida);
-    
-    return true;
-}
-
-
-function showErrorMessage(elem, msg) {
-    elem.parentNode.querySelector("small").innerHTML = msg + "<br>";
-    //form.querySelectorAll("small")[num].innerHTML = msg + "<br>";
-    elem.className = "error";
-    return false;
-}
-
-
-function auxBordiNeri(inputElem) {
-    if (inputElem.className == "error") {
-        inputElem.parentNode.querySelector("small").innerText = "";
-        inputElem.className = "";
-    }
-
-    return;
-}
-
-
-let campiInput = form.querySelectorAll("input");
-
-for (let i=0; i < campiInput.length-2; i++) {
-   campiInput[i].addEventListener("click", () => {
-        auxBordiNeri(campiInput[i]);
+    document.getElementById("password1").addEventListener("focus", () => {
+        console.log("tua mamma chrome");
+        return document.getElementById("sicurezzaPassword").style.display = "flex";
     });
-}
+
+    document.getElementById("password1").addEventListener("blur", () => {
+        return document.getElementById("sicurezzaPassword").style.display = "none";
+    });
 
 
-document.getElementById("mostra").addEventListener("click", mostraPassword);
+    function validitaPassword() {
+        let minuscole = /[a-z]/g;
+        let maiuscole = /[A-Z]/g;
+        let numeri = /[0-9]/g;
+        let speciali = /[!@#\$%\^\&*\)\(+=._-]/g;
 
-function mostraPassword() {
-    let p1 = document.getElementById("password1");
-    let p2 = document.getElementById("password2");
-    
-    if (p1.type === "password") {
-      p1.type = "text";
-      p2.type = "text";
+        //let regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{8,}$/g; 
+        //let regexMinima = /^[a-zA-Z]{8,}$/g
 
-    } 
-    
-    else {
-      p1.type = "password";
-      p2.type = "password";
-    }
-    
-    return;
-  }
+        let p1 = document.getElementById("password1").value;
+        
 
+        let testMaiuscole = maiuscole.test(p1);
+        let testMinuscole = minuscole.test(p1);
+        let testNumeri = numeri.test(p1);
+        let testSpeciali = speciali.test(p1);
 
-document.getElementById("password1").addEventListener("keyup", validitaPassword);
+        let testModerato = testMaiuscole && testMinuscole && p1.length >= 8;
+        let testForte = testModerato && testNumeri && testSpeciali;
 
-document.getElementById("password1").addEventListener("focus", () => {
-    return document.getElementById("sicurezzaPassword").style.display = "flex";
-});
+        let colorPass1, colorPass2, colorPass3;
+        let testoPass;
 
-document.getElementById("password1").addEventListener("blur", () => {
-    return document.getElementById("sicurezzaPassword").style.display = "none";
-});
+        if (testForte) {
+            testoPass = "Forte";
+            colorPass1 = "green";
+            colorPass2 = colorPass1;
+            colorPass3 = colorPass1;
+        }
 
-
-function validitaPassword() {
-    let minuscole = /[a-z]/g;
-    let maiuscole = /[A-Z]/g;
-    let numeri = /[0-9]/g;
-    let speciali = /[!@#\$%\^\&*\)\(+=._-]/g;
-
-    //let regex = /^[a-zA-Z0-9!@#\$%\^\&*\)\(+=._-]{8,}$/g; 
-    //let regexMinima = /^[a-zA-Z]{8,}$/g
-
-    let p1 = document.getElementById("password1").value;
-
-    let testMaiuscole = maiuscole.test(p1);
-    let testMinuscole = minuscole.test(p1);
-    let testNumeri = numeri.test(p1);
-    let testSpeciali = speciali.test(p1);
-
-    let testModerato = testMaiuscole && testMinuscole && p1.length >= 8;
-    let testForte = testModerato && testNumeri && testSpeciali;
-
-    let colorPass1, colorPass2, colorPass3;
-    let testoPass;
-
-    if (testForte) {
-        testoPass = "Forte";
-        colorPass1 = "green";
-        colorPass2 = colorPass1;
-        colorPass3 = colorPass1;
-    }
-
-    else if (testModerato) {
-        testoPass = "Moderata";
-        colorPass1 = "orange";
-        colorPass2 = colorPass1;
-        colorPass3 = "white";
-    }
-    
-    
-    else {
-        testoPass = "Debole<br> (Non accettabile, inserire almeno 8 caratteri di cui almeno una maiuscola)";
-        colorPass1 = "red";
-        colorPass2 = "white";
-        colorPass3 = colorPass2;
-    }
+        else if (testModerato) {
+            testoPass = "Moderata";
+            colorPass1 = "orange";
+            colorPass2 = colorPass1;
+            colorPass3 = "white";
+        }
+        
+        
+        else {
+            testoPass = "Debole<br> (Non accettabile, inserire almeno 8 caratteri di cui almeno una maiuscola)";
+            colorPass1 = "red";
+            colorPass2 = "white";
+            colorPass3 = colorPass2;
+        }
 
 
-    document.getElementById("commentoPassword").innerHTML = testoPass;
+        document.getElementById("commentoPassword").innerHTML = testoPass;
         document.getElementById("commentoPassword").style.color = colorPass1;
         document.getElementById("pass-forte").style.backgroundColor = colorPass3;
         document.getElementById("pass-media").style.backgroundColor = colorPass2;
         document.getElementById("pass-debole").style.backgroundColor = colorPass1;
 
-    return;
-
-}
+        return;
+    }
 
 
--- registrazione/index.php
+++ registrazione/index.php
@@ -2,106 +2,130 @@
 <html lang="it">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap CSS -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- My CSS -->
-    <link rel="stylesheet" href="../css/style.css">
-     
+    
     <title>Mosaic |Registrazione</title>
+    
+        <!-- Bootstrap CSS -->
+        <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    <!--favicons-->
-    <?php
-        include '../favicon.php';
-    ?>
-    <!--favicons-->
+        <!-- My CSS -->
+        <link rel="stylesheet" href="../css/style.css">
+
+        <link href="../css/sign-up.css" rel="stylesheet">
+    
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <?php
-        include '../navbar.php';
-        if(isset($_SESSION['utente'])) header("refresh:0;url=../index.php");
-    ?>
-    <div class="container-fluid my-container">
-
-        <div id="divregistrazione">
-            <h2>Registrazione</h2>
-            <form action="registar.php" method="POST" id="formregistrazione" onsubmit="return verificaForm()"> 
-                
-                <div class = "campoform">
-                    <label for="nome">Nome:</label><br>
-                    <input type="text" id="nome" name="nome" placeholder="Nome"><br>
-                    <small></small>
-                </div>
-                
-                <div class = "campoform">
-                    <label for="cognome">Cognome:</label><br>
-                    <input type="text" id="cognome" name="cognome" placeholder="Cognome"><br>
-                    <small></small>
-                </div>
-                
-                <div class = "campoform">
-                    <label for="username">Username:</label><br>
-                    <input type="text" id="username" name="username" placeholder="Username"><br>
-                    <small></small>
-                </div>
-                
-                <div style="display: flex; justify-content: center;">
-                    <div class = "campoform">
-                        <label for="password1">Crea una password:</label><br>
-                        <input type="password" id="password1" name="password1" placeholder="Password" title="La password dovrebbe contenere almeno 8 caratteri di cui almeno uno : minuscolo, maiuscolo, numerico e speciale"><br>
-                        <small></small> 
-
-                        <input type="checkbox" id = "mostra">
-                        <i>Mostra Password</i><br>
-
-                    </div>
-
-
-                    <div style="width: 5%;"></div><br>
-
-                    <div class = "campoform">
-                        <label for="password2">Conferma la password:</label><br>
-                        <input type="password" id="password2" name="password2" placeholder="Ripeti Password"><br>
-                        <small></small>
-                    </div>
-                </div>
-
-                <div id="commentoPassword"></div>
-
-                <div id="sicurezzaPassword">
-                    <div id="pass-debole"></div> <div id="pass-media"></div> <div id="pass-forte"></div>
-                </div>
-
-                <div class = "campoform">      
-                    <label for="email">Il tuo indirizzo email:</label><br>
-                    <input type="text" id=email name="email" placeholder="Email"><br>
-                    <small></small>
-                </div>
-                
-                <div>
-                    <input type="checkbox" name="checktermini" id="checktermini">
-                    <label style="display:inline" for="checktermini">Accetto i <a href="termini-servizio.php"><strong>Termini di servizio</strong></a> di Mosaic</label><br><br>
-                    <small></small>
-
-                    <input class="buttonSubmit" type="submit" value="Registrati">
-                </div>
-
-                <br>
-
-
-                <p>Hai già un account? <a href="../login">Sign in</a>.</p>
-
+    <div class="my-container">
+        <div style="background-color: #0d1117af; filter: blur(5px);position: fixed;top:0;left: 0; width: 100%; height: 100%;">
             
-            </form>
-
-
         </div>
-    </div><!-- CONTAINER END -->
-    <?php
-        include '../footer.php';
-    ?>
+        <div style="position: fixed; top:30%; left: 15%;width:70% ;height: 40%; border: 1px solid black; border-radius: 20px; display: flex; background-color: rgb(255, 255, 255); height: fit-content;">
+                
+            <div style="display:flex; justify-content: center; align-items: center; flex: 40%;">
+                <!--<img style="height: 400px; width: 400px;" src="../../../Backgrounds/istockphoto-666262220-612x612.jpeg">-->
+                
+                <div style = "display: flex; flex-direction: column;">
+                    <div class = "logo-blocchi" style="display: flex;">
+                        <div></div>
+                        <div style="background-color: #ffffff;"></div>
+                        <div style = "transform: rotate(20deg);"></div>
+                    </div>
+                    
+                    <div class = "logo-blocchi" style="display: flex;">
+                        <div></div>
+                        <div style = "height:2.5rem; align-self: flex-start"></div>
+                        <div></div>
+                    </div>
+                    
+                    <div class = "logo-blocchi" style="display: flex;">
+                        <div></div>
+                        <div style="background-color: #ffffff;"></div>
+                        <div></div>
+                    </div>
+                </div>
 
+            </div>
+
+
+            <div style="flex:60%;display: flex; flex-direction: column; background-color: rgb(207, 207, 207);border-radius: 0px 20px 20px 0px;">
+                <div style="flex: 15%;text-align: center;">
+                    <span style="font-size: 2vw;">Registrazione</span>
+                </div>
+                <div style="flex: 50%; display: flex;">
+                    <div style="flex: 0%;"></div>
+                    <div style="flex: 40%;display: flex;flex-direction: column; display: flex;flex-direction:column ;">
+                        
+                        <form action="registar.php" method="POST" id="formregistrazione" onsubmit="return verificaForm()" ></form>
+
+                        <span class="campoform" style="display: flex;">
+                            <label for="nome">Nome</label>
+                            <input id="nome" name="nome" type="text" placeholder="Nome" form="formregistrazione">
+                            <small></small>
+                        </span>
+                        
+                        <span class="campoform" style="display: flex;">
+                            <label for="cognome">Cognome</label>
+                            <input id="cognome" name="cognome" type="text" placeholder="Cognome" form="formregistrazione">
+                            <small></small>
+                        </span>
+
+                        <span class = "campoform" style="display: flex;">
+                            <label for="username"> Username: </label>
+                            <input type = "text" id="username" name="username" placeholder="Username" form="formregistrazione">
+                            <small></small>
+                        </span>
+
+                        <span class="campoform" style="display: flex;">
+                            <label for="email">Email</label>
+                            <input id="email" name="email" type="text" placeholder="Email" form="formregistrazione">
+                            <small></small>
+                        </span>
+
+                        <div style="flex: 0%;">
+                            <span  class="campoform" style="display: flex; position: relative;">
+                                <label for="password1">Password</label>
+                                <input id="password1" name="password1" type="text"  class= "tooltip" placeholder="Password" form="formregistrazione">
+                                <i class="fa fa-eye" id="mostra" aria-hidden="true"></i>
+                                <div id="commentoPassword" class="tooltiptext"></div>
+                                <small></small>
+                            </span>
+
+
+                            
+
+                            <div id="sicurezzaPassword">
+                                <div id="pass-debole"></div> <div id="pass-media"></div> <div id="pass-forte"></div>
+                            </div>
+
+
+                            <span class="campoform" style="display: flex;">
+                                <label for="password2">Conferma</label>
+                                <input id="password2" name="password2" type="text" placeholder="Conferma password" form="formregistrazione">
+                                <small></small>
+                            </span>
+                            
+                        </div>
+                        <span class="check_termini">
+                            <input type="checkbox" name="checktermini" id="checktermini" form="formregistrazione">
+                            <label style="margin-left:8px;" for="checktermini" form="formregistrazione">Accetto i <a href="termini-servizio.html"><strong>Termini di servizio</strong></a> di Mosaic</label>
+                            
+                        </span>
+                        
+                        <input class="buttonSubmit" type="submit" value="Registrati" form="formregistrazione">
+
+                    </div>
+                    <div style="flex: 0%;"></div>
+                </div>
+                <div style="flex:20%;">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
     <script src="../js/verifica-campi-inseriti.js"></script>
+
 </body>
 </html>
