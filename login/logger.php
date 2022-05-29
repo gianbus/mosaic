1e7751c19fab373ff09b4c7eff53b09782e1a8e5
Gianpaolo Busillo <gpbusillo@gmail.com>
2022-05-29 21:57:12 +0200

    Update logger.php

--- login/logger.php
+++ login/logger.php
@@ -2,190 +2,194 @@
     include '../config.php';
 
     $err = 0;
-    $message
+    $message = "";
 
-    if(isset($_COOKIE['utente'])) header( "refresh:0;url=../index.php" );
-
-    //IMPORTO USERNAME E PASSWORD CON REAL ESCAPE STRING + PROTEZIONE SQL INJECTION
-    $username = mysqli_real_escape_string($mysqli, $_POST['username']);
-    $password = mysqli_real_escape_string($mysqli, $_POST['password']);
-
-    //CRIPTO LA PASSWORD
-    $password = hash('sha256', $password);
-    
-    if($username == "" || $password == ""){ //CONTROLLO SIANO STATI INSERITI TUTTI I CAMPI
-        $err = 4;
-        $mess = "Attenzione, devi compilare tutti i campi!";
-        
-    }else if(!preg_match("/[a-zA-Z0-9._]+$/",$username) || preg_match("/[ ]+/",$username)){ //CONTROLLO FORMATO USERNAME
-        $err = 3;
-        $mess = "Username o Password errati!";
-    
+    if(isset($_COOKIE['utente'])){ //SE L'UTENTE E' GIA' LOGGATO
+        $err = 6;
+        $message = "Hai già effettuato il login!!";
     }else{
-        //VERIFICO L'ESISTENZA DELL'UTENTE CON QUELL'USERNAME E DI CONSEGUENZA LA PASSWORD
-        $recuperauser = mysqli_query($mysqli, "SELECT * FROM utenti WHERE username='$username' AND password='$password' ");
 
-        if($recuperauser){ //SE LA QUERY E' ANDATA A BUON FINE
-            //VERIFICO L'ESISTENZA DELL'ACCOUNT
-            $contauser = mysqli_num_rows($recuperauser);
+        //IMPORTO USERNAME E PASSWORD CON REAL ESCAPE STRING + PROTEZIONE SQL INJECTION
+        $username = mysqli_real_escape_string($mysqli, $_POST['username']);
+        $password = mysqli_real_escape_string($mysqli, $_POST['password']);
 
-            if($contauser == 1){//SE LE CREDENZIALI INSERITE SONO CORRETTE
-                $sessione = mysqli_fetch_array($recuperauser);
+        //CRIPTO LA PASSWORD
+        $password = hash('sha256', $password);
+        
+        if($username == "" || $password == ""){ //CONTROLLO SIANO STATI INSERITI TUTTI I CAMPI
+            $err = 4;
+            $mess = "Attenzione, devi compilare tutti i campi!";
+            
+        }else if(!preg_match("/[a-zA-Z0-9._]+$/",$username) || preg_match("/[ ]+/",$username)){ //CONTROLLO FORMATO USERNAME
+            $err = 3;
+            $mess = "Username o Password errati!";
+        
+        }else{
+            //VERIFICO L'ESISTENZA DELL'UTENTE CON QUELL'USERNAME E DI CONSEGUENZA LA PASSWORD
+            $recuperauser = mysqli_query($mysqli, "SELECT * FROM utenti WHERE username='$username' AND password='$password' ");
 
-                if($sessione['verificato']==1){ //SE L'UTENTE E' UN UTENTE VERIFICATO
+            if($recuperauser){ //SE LA QUERY E' ANDATA A BUON FINE
+                //VERIFICO L'ESISTENZA DELL'ACCOUNT
+                $contauser = mysqli_num_rows($recuperauser);
 
-                    //ESEGUE L'ACCESSO SETTANDO I DUE COOKIES "UTENTE" E "PUNTI" PER LA DURATA DI UN'ORA
+                if($contauser == 1){//SE LE CREDENZIALI INSERITE SONO CORRETTE
+                    $sessione = mysqli_fetch_array($recuperauser);
 
-                    setcookie("utente", $sessione['username'], [
-                        'expires' => time() + 3600,
-                        'path' => '/',
-                        'domain' => 'localhost',
-                        'secure' => true,
-                        'httponly' => true,
-                        'samesite' => 'Strict',
-                    ]);
+                    if($sessione['verificato']==1){ //SE L'UTENTE E' UN UTENTE VERIFICATO
 
-                    setcookie("punti", $sessione['punti'], [
-                        'expires' => time() + 3600,
-                        'path' => '/',
-                        'domain' => 'localhost',
-                        'secure' => true,
-                        'httponly' => true,
-                        'samesite' => 'Strict',
-                    ]);
+                        //ESEGUE L'ACCESSO SETTANDO I DUE COOKIES "UTENTE" E "PUNTI" PER LA DURATA DI UN'ORA
 
-                }else{ //SE NON E' VERIFICATO
-                    $mess="Il tuo account non risulta verificato!";
+                        setcookie("utente", $sessione['username'], [
+                            'expires' => time() + 3600,
+                            'path' => '/',
+                            'domain' => 'localhost',
+                            'secure' => true,
+                            'httponly' => true,
+                            'samesite' => 'Strict',
+                        ]);
 
-                    //GENERAZIONE NUOVO CODICE CONFERMA RANDOMICO
-                    $codiceconferma = md5(uniqid(rand()));
+                        setcookie("punti", $sessione['punti'], [
+                            'expires' => time() + 3600,
+                            'path' => '/',
+                            'domain' => 'localhost',
+                            'secure' => true,
+                            'httponly' => true,
+                            'samesite' => 'Strict',
+                        ]);
 
-                    //AGGIORNO IL CODICE CONFERMA SUL DB PER TALE UTENTE
-                    $oldcodice = $sessione['codiceconferma'];
-                    $aggiornacodice = mysqli_query($mysqli, "UPDATE utenti SET codiceconferma = '$codiceconferma' WHERE codiceconferma = '$oldcodice' and verificato=0");
-                    
-                    if($aggiornacodice){ //AGGIORNAMENTO ANDATO A BUON FINE
+                    }else{ //SE NON E' VERIFICATO
+                        $mess="Il tuo account non risulta verificato!";
 
-                        $to=$sessione['email'];
-                        $nome = $sessione['nome'];
-                        $cognome = $sessione['cognome'];
-                        $username = $sessione['username'];
-                        $oggetto='Registrazione Mosaic';
-                        $message="<html>
-                                <head>
-                                <style>
-                                    #email-body{
-                                        background-color: #161b22;
-                                        text-align: center;
-                                        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-                                        padding: 30px;
-                                        border-radius: 20px;
-                                        width: fit-content;
-                                        margin:auto;
-                                    }
-                    
-                                    h3, h4{
-                                        color: rgb(255, 255, 255);
-                                        font-weight: 500;
-                                        margin:10px;
-                                    }
-                                    
-                                    #bottone{
-                                        background-color: transparent;
-                                        border-radius: 5px;
-                                        border: 0px;
-                                        background-image: linear-gradient(-75deg, #3a3d42, rgba(0, 34, 73, 0.843));
-                                        color: white;
-                                        padding: 5px;
-                                        width:fit-content;
-                                        margin:auto;
-                                    }
-                    
-                                    p{
-                                        color: rgb(255, 255, 255);
-                                    }
-                    
-                                    #testo{
-                                        margin: 15px auto;
-                                        padding: 20px;
-                                        border-radius: 20px;
-                                        width: fit-content;
-                                        background-color: rgba(255, 255, 255, 0.034);
-                                        border: 1px solid rgba(255, 255, 255, 0.221);
-                                    }
-                    
-                                    #bottone:hover{
-                                        cursor: pointer;
-                                        background-image: linear-gradient(75deg, #3a3d42, rgba(0, 34, 73, 0.843));
-                                        background-color: transparent;
-                                    }
-                    
-                                    .link-attivazione{
-                                        text-decoration:none;
-                                        color: white;
-                                    }
-                    
-                                </style>
-                                </head>
-                    
-                            <body>
-                                <div id='email-body'>
-                                    <img width='300px' src='https://www.ltw-mosaic.it/assets/logos/logo2.png'>
-                                    <div id='testo'>
-                                        <h3>Benvenuto $nome $cognome!</h3>
-                                        <h4>Grazie per esserti iscritto a Mosaic!</h4>
-                    
-                                        <p> Ecco i tuoi dati per accedere al sito:<br>
-                                            <b>Username:</b> <i>$username</i><br>
-                                            <b>Password:</b> <i>INSERITA IN FASE DI REGISTRAZIONE</i></p>
-                    
-                                            <a class='link-attivazione' href='https://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma'><div id='bottone'>Clicca qui per attivare il tuo account.</div></a>
+                        //GENERAZIONE NUOVO CODICE CONFERMA RANDOMICO
+                        $codiceconferma = md5(uniqid(rand()));
+
+                        //AGGIORNO IL CODICE CONFERMA SUL DB PER TALE UTENTE
+                        $oldcodice = $sessione['codiceconferma'];
+                        $aggiornacodice = mysqli_query($mysqli, "UPDATE utenti SET codiceconferma = '$codiceconferma' WHERE codiceconferma = '$oldcodice' and verificato=0");
+                        
+                        if($aggiornacodice){ //AGGIORNAMENTO ANDATO A BUON FINE
+
+                            $to=$sessione['email'];
+                            $nome = $sessione['nome'];
+                            $cognome = $sessione['cognome'];
+                            $username = $sessione['username'];
+                            $oggetto='Registrazione Mosaic';
+                            $message="<html>
+                                    <head>
+                                    <style>
+                                        #email-body{
+                                            background-color: #161b22;
+                                            text-align: center;
+                                            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+                                            padding: 30px;
+                                            border-radius: 20px;
+                                            width: fit-content;
+                                            margin:auto;
+                                        }
+                        
+                                        h3, h4{
+                                            color: rgb(255, 255, 255);
+                                            font-weight: 500;
+                                            margin:10px;
+                                        }
                                         
-                                    </div>
-                                    <div>
-                                        <p style='color:#ccc; font-size:12px;'>Se il bottone non funziona clicca sul link o copia e incolla sul browser:<br>
-                                        <a class='link-attivazione' style='color:#ccc;' href='https://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma'>http://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma</a></p><br>
-                                        <p>MOSAIC &copy; 2022</p>
-                                    </div>
-                                </div>
-
-                                
-                            </body>
-                    
-                            </html>";
+                                        #bottone{
+                                            background-color: transparent;
+                                            border-radius: 5px;
+                                            border: 0px;
+                                            background-image: linear-gradient(-75deg, #3a3d42, rgba(0, 34, 73, 0.843));
+                                            color: white;
+                                            padding: 5px;
+                                            width:fit-content;
+                                            margin:auto;
+                                        }
                         
-                        $headers[] = 'MIME-Version: 1.0';
-                        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-                        $headers[] = "To: <$email>";
-                        $headers[] = 'From: "Mosaic" <no-reply@ltw-mosaic.it>';
-
-                        $sentmail=mail($to, $oggetto, $message, implode("\r\n", $headers));
+                                        p{
+                                            color: rgb(255, 255, 255);
+                                        }
                         
-                        if($sentmail){
-                            $err = 1;
-                            $mess .= "Ti abbiamo inviato nuovamente il link di verifica!<br>Clicca sul link per procedere all'attivazione.";
+                                        #testo{
+                                            margin: 15px auto;
+                                            padding: 20px;
+                                            border-radius: 20px;
+                                            width: fit-content;
+                                            background-color: rgba(255, 255, 255, 0.034);
+                                            border: 1px solid rgba(255, 255, 255, 0.221);
+                                        }
+                        
+                                        #bottone:hover{
+                                            cursor: pointer;
+                                            background-image: linear-gradient(75deg, #3a3d42, rgba(0, 34, 73, 0.843));
+                                            background-color: transparent;
+                                        }
+                        
+                                        .link-attivazione{
+                                            text-decoration:none;
+                                            color: white;
+                                        }
+                        
+                                    </style>
+                                    </head>
+                        
+                                <body>
+                                    <div id='email-body'>
+                                        <img width='300px' src='https://www.ltw-mosaic.it/assets/logos/logo2.png'>
+                                        <div id='testo'>
+                                            <h3>Benvenuto $nome $cognome!</h3>
+                                            <h4>Grazie per esserti iscritto a Mosaic!</h4>
+                        
+                                            <p> Ecco i tuoi dati per accedere al sito:<br>
+                                                <b>Username:</b> <i>$username</i><br>
+                                                <b>Password:</b> <i>INSERITA IN FASE DI REGISTRAZIONE</i></p>
+                        
+                                                <a class='link-attivazione' href='https://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma'><div id='bottone'>Clicca qui per attivare il tuo account.</div></a>
+                                            
+                                        </div>
+                                        <div>
+                                            <p style='color:#ccc; font-size:12px;'>Se il bottone non funziona clicca sul link o copia e incolla sul browser:<br>
+                                            <a class='link-attivazione' style='color:#ccc;' href='https://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma'>http://ltw-mosaic.it/registrazione/attiva-account.php?passkey=$codiceconferma</a></p><br>
+                                            <p>MOSAIC &copy; 2022</p>
+                                        </div>
+                                    </div>
 
-                        }else{	                        
-                            $err = 2;
-                            $mess .= "Si è vericato un errore nell'invio della mail con il link di attivazione! Riprova ad accedere più tardi!";
+                                    
+                                </body>
+                        
+                                </html>";
+                            
+                            $headers[] = 'MIME-Version: 1.0';
+                            $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+                            $headers[] = "To: <$email>";
+                            $headers[] = 'From: "Mosaic" <no-reply@ltw-mosaic.it>';
+
+                            $sentmail=mail($to, $oggetto, $message, implode("\r\n", $headers));
+                            
+                            if($sentmail){
+                                $err = 1;
+                                $mess .= "Ti abbiamo inviato nuovamente il link di verifica!<br>Clicca sul link per procedere all'attivazione.";
+
+                            }else{	                        
+                                $err = 2;
+                                $mess .= "Si è vericato un errore nell'invio della mail con il link di attivazione! Riprova ad accedere più tardi!";
+                            }
+
+                        }else{ //AGGIORNAMENTO NON ANDATO A BUON FINE
+                            $err = 5;
+                            $mess .= "Si è verificato un errore. Riprova più tardi.";
                         }
 
-                    }else{ //AGGIORNAMENTO NON ANDATO A BUON FINE
-                        $err = 5;
-                        $mess .= "Si è verificato un errore. Riprova più tardi.";
                     }
 
+                }else{ //SE LE CREDENZIALI SONO ERRATE
+                    $err = 4;
+                    $mess="Username o Password errati!";
                 }
 
-            }else{ //SE LE CREDENZIALI SONO ERRATE
-                $err = 4;
-                $mess="Username o Password errati!";
-            }
-
-        }else{ //SE SI SONO VERIFICATI ERRORI DURANTE L'ESECUZIONE DELLA QUERY
-            $err = 5;
-            $mess = "Si è verificato un errore durante l\'esecuzione della query!";
-        }			
+            }else{ //SE SI SONO VERIFICATI ERRORI DURANTE L'ESECUZIONE DELLA QUERY
+                $err = 5;
+                $mess = "Si è verificato un errore durante l\'esecuzione della query!";
+            }			
+        }
     }
 
     $resp = new stdClass();
